872d066cc84c5b7d3395771a56d149b42d209f10
Felipe Sampaio <felipe.silva@sou.inteli.edu.br>
2024-10-17 19:22:20 -0300

    teste2

--- config/db.js
+++ config/db.js
@@ -2,24 +2,29 @@
 
 const { Sequelize } = require('sequelize');
 
-// Carrega as variáveis de ambiente apenas em desenvolvimento
+// Carrega as variáveis de ambiente
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
 
+// Configuração de conexão com PostgreSQL no Render.com
 const sequelize = new Sequelize(
-  process.env.DB_NAME,     // Nome do banco de dados
-  process.env.DB_USER,     // Usuário do banco de dados
-  process.env.DB_PASS,     // Senha do banco de dados
+  process.env.DB_NAME || 'mayo_sql',      // Nome do banco de dados
+  process.env.DB_USER || 'mayo_sql_user', // Nome do usuário do banco de dados
+  process.env.DB_PASS || 'UG3HvPls83cAH6nwoV0E2Cuq2lvB7SbQ', // Senha do banco de dados
   {
-    host: process.env.DB_HOST, // Host do banco de dados
-    port: process.env.DB_PORT, // Porta do banco de dados
-    dialect: 'mysql',
-    logging: false // Desativa logs detalhados (opcional)
+    host: process.env.DB_HOST || 'dpg-cs8ols5ds78s738j04kg-a',  // Hostname do banco de dados no Render
+    port: process.env.DB_PORT || 5432,  // Porta do PostgreSQL (geralmente 5432)
+    dialect: 'postgres',        // Tipo de banco de dados (PostgreSQL)
+    dialectOptions: {
+      // Se o Render.com exigir SSL para o banco de dados, configure isso
+      ssl: process.env.NODE_ENV === 'production' ? { require: true, rejectUnauthorized: false } : false
+    },
+    logging: false  // Desativa logs detalhados
   }
 );
 
-// Verificar a conexão com o banco de dados
+// Verifica a conexão com o banco de dados
 sequelize.authenticate()
   .then(() => {
     console.log('Conexão com o banco de dados foi bem-sucedida.');
