6166bf7e7a33d4f1a2bdb640e89dc8b89fa7b9c7
Felipe Sampaio <felipe.silva@sou.inteli.edu.br>
2024-10-17 18:31:52 -0300

    nova versã

--- config/db.js
+++ config/db.js
@@ -1,11 +1,27 @@
 // config/db.js
 
+require('dotenv').config(); // Carrega as variáveis de ambiente
+
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('todo_list_db', 'root', 'ALMIr6221', {
-  host: 'localhost',
-  dialect: 'mysql'
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,     // Nome do banco de dados
+  process.env.DB_USER,     // Usuário do banco de dados
+  process.env.DB_PASS,     // Senha do banco de dados
+  {
+    host: process.env.DB_HOST, // Host do banco de dados
+    port: process.env.DB_PORT, // Porta do banco de dados
+    dialect: 'mysql',
+    dialectOptions: {
+      // Se o seu provedor exigir SSL, configure aqui
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    logging: false // Desativa logs detalhados (opcional)
+  }
+);
 
 // Verificar a conexão com o banco de dados
 sequelize.authenticate()
